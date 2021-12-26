00035fa320942100f8f043453e4d5e5ffdd2c041
KissmeDragonDD <saokimtoi147@gmail.com>
2021-12-26 16:06:42 +0700

    test

--- package-info.java
+++ package-info.java
@@ -18,7 +18,7 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 @ParametersAreNonnullByDefault
-package org.sonar.plugins.surefire;
+package org.sonar.plugins.surefire.data;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
